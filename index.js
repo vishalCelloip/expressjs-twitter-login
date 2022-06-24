483fb2c2eab556ac750d7f1fc504014712277721
vishalCelloip <97949324+vishalCelloip@users.noreply.github.com>
2022-06-24 10:20:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 require('dotenv').config();
 
 const path = require('path');
-const express = require('express');
+const express = require('experess');
 const passport = require('passport');
 const { Strategy } = require('passport-twitter');
 const { TWITTER_CONSUMER_KEY, TWITTER_CONSUMER_SECRET, SESSION_SECRET } =  process.env;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 const app = express();
 const routes = require('./routes');
 
